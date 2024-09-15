9bdec7689022ab7de24956489a632db5a73fed89
aengusk <116749459+aengusk@users.noreply.github.com>
2024-09-15 09:39:47 -0400

    bug testing and fixes to scrabblegamestate.py and test.py, added *.pyc to .gitignore

--- scrabblegamestate.py
+++ scrabblegamestate.py
@@ -9,19 +9,24 @@ except ImportError:
             return True
 
 class boardstate:
-    def __init__(self, dimension, sequence_ID):
+    def __init__(self, dimension, seq_ID='A001477'):
         self.dimension = dimension
         self.data = [[None] * self.dimension] * self.dimension
-        self.sequence_manager = SequenceManager(seq_id=sequence_ID)
-        pass
+        self.sequence_manager = SequenceManager(seq_id='A001477')
+        # seq_id='A001477' for the list of all nonnegative integers
     
     # We expect 'move' to be a tuple of 3-tuples where each 3-tuple is a square
     # in the form (row, col, digit)
 
-    def does_move_fit(self, move):
+    def does_move_fit(self, move, verbose=False):
         for square in move:
             row, col, _ = square
-            if self.data[row][col] is not None:
+            try:
+                if self.data[row][col] is not None:
+                    print("a space is already taken {}\n".format(move) if verbose else '', end='')
+                    return False
+            except IndexError:
+                print("the move is off the board {}\n".format(move) if verbose else '', end='')
                 return False
         return True
     
@@ -32,15 +37,15 @@ class boardstate:
             data_next[row][col] = digit
         return data_next
 
-    def numbers_from_row(self, row):
+    def numbers_from_row(self, row, verbose=False):
         # this takes in a row or column, in the form [1, 2, 6, None, None, 5, None, None] for example
         # and outputs the numbers that need to be tested, for example [126]
         string_expression = ''.join((str(i) if i is not None else '_' for i in row))
         strings = string_expression.split('_')
         strings = tuple(string for string in strings if len(string) >= 2)
-        return tuple(int(i) for i in strings)
+        return strings
 
-    def is_number_legal(self, move):
+    def is_number_legal(self, move, verbose=False):
         self.data_next = self.return_next_data(move)
         if move[0][0] == move[1][0]: # the move is a row move
             row_in_question = list(self.data_next[move[0][0]])
@@ -51,9 +56,12 @@ class boardstate:
         else:
             raise ValueError('The move is not confined to one row or one column I think')
         numbers_to_test = self.numbers_from_row(row_in_question)
+        print('testing these numbers: {}\n'.format(numbers_to_test) if verbose else '', end='')
         for number in numbers_to_test:
-            if not self.sequence_manager.verify_number(number):
+            if not self.sequence_manager.verify_number(int(number)):
+                print('this number failed: {}\n'.format(number) if verbose else '', end='')
                 return False
+        print('they all passed\n' if verbose else '', end='')
         return True
 
         # number_submitted_as_string = ''
@@ -66,8 +74,7 @@ class boardstate:
         # number_submitted = int(number_submitted_as_string)
         # return self.sequence_manager.verify_number(number_submitted)
 
-    def is_move_legal(self, move):
-        return self.does_move_fit(move) and self.is_number_legal(move)
+
 
     def __repr__(self): 
         return 'boardstate({})'.format(self.dimension)
@@ -98,33 +105,78 @@ class boardstate:
                     board_str+=str(number) + ' '
             board_str+='\n'
         return board_str
+    
+    def __str__(self):
+        return self.to_str()
 
     # ##############################
-    # is_move_valid and update_board_with_move
+    # is_move_legal and update_board_with_move
     # are the only two functions that need to be
     # accessed from outside the class. 
     # ##############################
-    def is_move_valid(self, move):
-        return self.does_move_fit(move) and self.is_number_legal(move)
-    
+    # def is_move_valid(self, move, verbose = False):
+    #     return self.does_move_fit(move, verbose=verbose) and self.is_number_legal(move, verbose=verbose)
+    def is_move_legal(self, move, verbose=False):
+        return self.does_move_fit(move, verbose=verbose) and self.is_number_legal(move, verbose=verbose)
+
+
     def update_board_with_move(self, move):
         # print(self.return_next_data(move))
         self.data = self.return_next_data(move)
 
-def debugging():
-    bs = boardstate(5, 'OEIScode')
-    bs.display()
+# def debugging():
+#     bs = boardstate(5, 'OEIScode')
+#     bs.display()
 
-    move = ((0,0,1), (0,1,9), (0, 2, 6))
-    legal = bs.is_move_legal(move)
-    if legal:
-        bs.update_board_with_move(move)
-    bs.display()
+#     move = ((0,0,1), (0,1,9), (0, 2, 6))
+#     legal = bs.is_move_legal(move, verbose=True)
+#     print(legal)
+#     if legal:
+#         bs.update_board_with_move(move)
+#     bs.display()
 
-    move = ((1,1,1), (2,1,1), (3,1,1))
-    legal = bs.is_move_legal(move)
-    if legal:
-        bs.update_board_with_move(move)
-    bs.display()
+#     move = ((1,1,1), (2,1,1), (3,1,1))
+#     legal = bs.is_move_legal(move, verbose=True)
+#     print(legal)
+#     if legal:
+#         bs.update_board_with_move(move)
+#     bs.display()
+
+#     move = ((2,4,7), (3,4,2), (4,4,9))
+#     legal = bs.is_move_legal(move, verbose=True)
+#     print(legal)
+#     if legal:
+#         bs.update_board_with_move(move)
+#     bs.display()
+
+#     move = ((2,2,6), (2,3,6))
+#     legal = bs.is_move_legal(move, verbose=True)
+#     print(legal)
+#     if legal:
+#         bs.update_board_with_move(move)
+#     bs.display()
+
+#     move = ((0,3,8), (1,3,8))
+#     legal = bs.is_move_legal(move, verbose=True)
+#     print(legal)
+#     if legal:
+#         bs.update_board_with_move(move)
+#     bs.display()
+
+#     move = ((3,2,0), (3,3,0))
+#     legal = bs.is_move_legal(move, verbose=True)
+#     print(legal)
+#     if legal:
+#         bs.update_board_with_move(move)
+#     bs.display()
+
+#     move = ((4,2,0), (4,3,0))
+#     legal = bs.is_move_legal(move, verbose=True)
+#     print(legal)
+#     if legal:
+#         bs.update_board_with_move(move)
+#     bs.display()
+
 if __name__ == '__main__':
-    debugging()
+    #debugging()
+    print('scrabblegamestate.py as __main__')
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from scrabblegamestate import boardstate
 class TestStringMethods(unittest.TestCase):
 
     def test_board(self):
-        bs = boardstate(5, 'A001477')
+        bs = boardstate(5, seq_ID='A001477')
         self.assertEqual(repr(bs.display()), repr(('_ '*5 +'\n')*5))
 
         move = ((0,0,1), (0,1,9), (0, 2, 6))
@@ -21,6 +21,64 @@ class TestStringMethods(unittest.TestCase):
         # bs.update_board_with_move(move)
         # self.assertEqual(repr(bs.display()), repr(('1 9 6 _ _ \n') + ('_ 1 _ _ _ \n')*2+('_ '*5 +'\n')*2))
         # # this case fails but not sure if we need the display function
+
+    def test_board_2(self): 
+        # this one uses no assertions and must be manually inspected
+        # 
+        bs = boardstate(5, seq_ID='A001477')
+        bs.display()
+
+        move = ((0,0,1), (0,1,9), (0, 2, 6))
+        legal = bs.is_move_legal(move, verbose=True)
+        print(legal)
+        if legal:
+            bs.update_board_with_move(move)
+        bs.display()
+
+        move = ((1,1,1), (2,1,1), (3,1,1))
+        legal = bs.is_move_legal(move, verbose=True)
+        print(legal)
+        if legal:
+            bs.update_board_with_move(move)
+        bs.display()
+
+        move = ((2,4,7), (3,4,2), (4,4,9))
+        legal = bs.is_move_legal(move, verbose=True)
+        print(legal)
+        if legal:
+            bs.update_board_with_move(move)
+        bs.display()
+
+        move = ((2,2,6), (2,3,6))
+        legal = bs.is_move_legal(move, verbose=True)
+        print(legal)
+        if legal:
+            bs.update_board_with_move(move)
+        bs.display()
+
+        move = ((0,3,8), (1,3,8))
+        legal = bs.is_move_legal(move, verbose=True)
+        print(legal)
+        if legal:
+            bs.update_board_with_move(move)
+        bs.display()
+
+        move = ((3,2,0), (3,3,0))
+        legal = bs.is_move_legal(move, verbose=True)
+        print(legal)
+        if legal:
+            bs.update_board_with_move(move)
+        bs.display()
+
+        move = ((4,2,0), (4,3,0))
+        legal = bs.is_move_legal(move, verbose=True)
+        print(legal)
+        if legal:
+            bs.update_board_with_move(move)
+        bs.display()
+
+    def test_board_3(self):
+        bs = boardstate(7, seq_ID='A000040')
 
 
     def test_player(self):
